1797196aed4d41872290b34c40d1f101660d2e19
Carlos Neves <carlos.amn.go@gmail.com>
2018-06-08 11:59:47 -0300

    Backend backbone and makefile to run postgres in dev.

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000')
+})
---
+++ makefile
@@ -0,0 +1,2 @@
+postgres:
+	docker run --name postgres-dev -p 5432:5432 -d SanConnect
