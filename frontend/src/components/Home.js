7ac735cab1f2775ef8c7595d14ecdf0ca810a33b
Guilherme Menegali <guilherme.menegali@gmail.com>
2018-06-25 10:03:58 -0300

    Playing with Home, SignIn, SignUp frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import Background from '../images/homeBackground.jpg'
 
 const Home = () => (
 	<div>
 		<nav class="pt-navbar pt-dark">
 			<div class="pt-navbar-group pt-align-left">
-		  		<Link to='/sign-in' style = {{textDecoration : 'none', color: 'white'}}><button class="pt-button pt-minimal">SanConnect</button></Link>
+		  		<Link to='/' style = {{textDecoration : 'none', color: 'white'}}><button class="pt-button pt-minimal">SanConnect</button></Link>
 			</div>
 			<div class="pt-navbar-group pt-align-right">
-			  <Link to='/'><button class="pt-button pt-intent-primary">Login</button></Link>
+			  <Link to='/sign-in'><button class="pt-button pt-intent-primary">Login</button></Link>
 			  <span class="pt-navbar-divider"></span>
 			  <Link to='/sign-up'><button type="button" class="pt-button pt-intent-success">Cadastre-se</button></Link>
 			</div>
-		</nav>  
+		</nav>
+		<center>
+		<div class="hero-image" style = {{backgroundImage: "url(" + Background + ")", backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundAttachment: 'fixed', backgroundSize: 1900, height: 650, align :'center'}}>
+		  <div class="hero-text">
+		  	<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+		    <h1 style = {{color : '#ff2121'}}>Conectando Pessoas</h1>
+		    <h5 style = {{color : '#ff2121'}}>Tornando o mundo mais divertido!</h5>
+		    <Link to='/sign-up'><button type="button" class="pt-button pt-intent-success">Cadastre-se</button></Link>
+		  </div>
+		</div>
+		</center>
+
+
+
 	</div>
 );
 
