139ec7ab54ae5d9aecaca06fd64e92871764480c
brunoxd <bruno.mendes.costa@usp.br>
2018-06-30 20:23:18 -0300

    Add updateUser functions

---
+++ backend/src/graphql/root/updateUser.js
@@ -0,0 +1,22 @@
+/* Função que atualiza as informações pessoais de um usuário. */
+async function updateUser({ user }, { pool, viewer }) {
+	if(viewer.email !== user.email) 
+		throw new Error('Usuário não autenticado.')
+
+	let query = {
+		text: 'UPDATE USUARIO SET NOME = $1, DESCRICAO = $2, FOTO = $3 WHERE EMAIL = $4',
+		values: [user.nome, user.descricao, user.foto, viewer.email]
+	}
+
+	try {
+		let res = await pool.query(query)
+		if(res.rowCount > 0)
+			return 'Informações alteradas com sucesso.'
+		else
+			return 'Falha na atualização das informações.'		
+	} catch(err) {
+		throw(err)
+	}
+}
+
+module.exports = updateUser
