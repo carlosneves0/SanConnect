45569cc14492dcd026b8bc5f4f532d9b44783d14
gamenegali <guilherme.menegali@usp.br>
2018-07-05 18:07:47 -0300

    Made UserView Frontend

--- frontend/src/components/UserView.js
+++ frontend/src/components/UserView.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Button, Image } from 'semantic-ui-react'
+import userImage from './Layout/Avatar/default-picture.jpg';
+
+const userName = ['Delbem da Silva']
+const userDescription = ['Sou um professor muito daorah que adora eletrônica. Tenho interesse em jogos.']
 
 class UserView extends React.Component {
-	render() {
-		return <h1>UserView</h1>
-	}
+  render() {
+    return (
+   	  <div>
+	      <center>
+	        <div style = {{paddingTop: 90, width: 600}}>
+	          <img src={userImage} style = {{height: 300, width: 300}} />
+	          <h1>{userName}</h1>
+
+	          <p>{userDescription}</p>
+
+	          <br></br><br></br>
+	          <Button type='submit' secondary style = {{width: 600}}>Voltar</Button>
+	        </div>
+	      </center>
+      </div>
+    )
+  }
 }
 
 export default UserView
