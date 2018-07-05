74907c4b1ffae4d92d161142c5efc90377a58f45
Carlos Neves <carlos.amn.go@gmail.com>
2018-07-05 10:35:17 -0300

    Improve MyEvents a bit.

--- frontend/src/components/MyEvents/MyEvents.js
+++ frontend/src/components/MyEvents/MyEvents.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Loader, Icon, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 import './MyEvents.css'
 
 const Event = ({
@@ -64,15 +65,21 @@ const MyEvents = ({ events, viewer }) => {
 
     if (data.length === 0) {
       return (
-        <div className='App-fixed-center'>
+        <div
+          className='App-fixed-center'
+          style={{ width: '90vw' }}
+        >
           <h1>Você não possui nenhum evento</h1>
-          <p><Button>Criar um novo evento</Button></p>
+          <p><Link to='/create-event'>
+            <Button>Criar um Evento</Button>
+          </Link></p>
         </div>
       )
     }
 
     return (
       <div className='MyEvents'>
+        <h2 style={{ textAlign: 'center', marginTop: '2vh' }}>Meus Eventos</h2>
         <ul>
           {data.map(
             (event, index) => <Event key={index} {...event} />
