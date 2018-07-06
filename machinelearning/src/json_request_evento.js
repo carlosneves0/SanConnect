286dbb62c43c3a1b4f400ef94e2ec9038b946807
guilherme <guilherme.milan9@gmail.com>
2018-07-05 23:15:44 -0300

    json de teste

---
+++ machinelearning/src/json_request_evento.js
@@ -0,0 +1,55 @@
+{
+	"usuario": {
+		"email": "joao@email.com",
+	    "preference": {
+	        "board games": 0.5,
+	        "festas": 0.1,
+	        "grupo de estudos": 0.2, 
+	        "videogames": 0.3,
+	        "filmes": 0.8
+	    }
+	},
+
+	"eventos":[
+		{
+			"id": 123,
+			"category":["board games", "grupo de estudos"]
+		},
+		{
+			"id": 124,
+			"category":["festas", "filmes"]
+		},
+				{
+			"id": 144,
+			"category":["board games","videogames"]
+		},
+		{
+			"id": 125,
+			"category":["festas"]
+		},
+		{
+			"id": 126,
+			"category":["filmes", "videogames"]
+		},
+		{
+			"id": 127,
+			"category":["festas","grupo de estudos"]
+		},
+		{
+			"id": 128,
+			"category":["videogames"]
+		},
+		{
+			"id": 129,
+			"category":["grupo de estudos", "filmes"]
+		},
+		{
+			"id": 130,
+			"category":["filmes"]
+		},
+		{
+			"id": 131,
+			"category":["board games"]
+		}																												
+	]
+}
